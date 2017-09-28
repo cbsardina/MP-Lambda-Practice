521b7d43095cae06dbbb3a2965174395071e2abf
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-09-28 00:04:43 -0400

    MP-Lambda Practice: Anon & Lambda written.

--- src/com/sardina/Main.java
+++ src/com/sardina/Main.java
@@ -1,8 +1,32 @@
 package com.sardina;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // !! - Write an anonymous Runnable class here
+        //It can just print out "Anonymous run" or similar
+         Runnable anonRun = new Runnable() {
+             @Override
+             public void run() {
+                 System.out.println("Anonymous run");
+             }
+         };
+        anonRun.run();
+
+        // !! - Write a lambda function here
+        //It can also print out something like "Lambda run"
+        Runnable lambdaRun = () -> {
+            System.out.println("Lambda Run");
+        };
+        lambdaRun.run();
+
+
+
+
+
+
+
     }
 }
